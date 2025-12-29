f60274db853f704521758fda5551508276883fbb
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2025-12-29 15:57:21 +0900

    기본설정

--- main.py
+++ main.py
@@ -1,6 +1,74 @@
-from fastapi import FastAPI
+from urllib import request
+import uvicorn
+from fastapi import FastAPI, Depends, Request, Form, Response, HTTPException, File, UploadFile, Body
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
+from fastapi.templating import Jinja2Templates
+from pycparser.ply.yacc import resultlimit
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from starlette.middleware.sessions import SessionMiddleware
+from fastapi.staticfiles import StaticFiles
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from sqlalchemy import text
+import dotenv
+import os
+import base64
+import datetime
+from PIL import Image, ImageFont, ImageDraw
+import io
+from pydantic import BaseModel
+from fastapi.responses import StreamingResponse
+from io import BytesIO
+from starlette.responses import FileResponse
+from pathlib import Path
+
+dotenv.load_dotenv()
+DATABASE_URL = os.getenv("dburl")
+engine = create_async_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_timeout=10,
+    pool_recycle=1800)
+
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 app = FastAPI()
+
+app.add_middleware(SessionMiddleware, secret_key="supersecretkey")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 도메인 허용
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/thumbnails", StaticFiles(directory="static/img/members/"), name="thumbnails")
+THUMBNAIL_DIR = "./static/img/members"
+BASE_DIR = Path(__file__).resolve().parent
+
+
+# 데이터베이스 세션 생성
+async def get_db():
+    async with async_session() as session:
+        yield session
+
+
+# 썸네일 생성 함수
+async def save_thumbnail(image_data: bytes, memberno: int, size=(100, 100)):
+    # 디렉토리가 없으면 생성
+    os.makedirs(THUMBNAIL_DIR, exist_ok=True)
+    # 원본 이미지를 Pillow로 열기
+    image = Image.open(io.BytesIO(image_data))
+    # 썸네일 생성
+    image.thumbnail(size)
+    # 저장 경로
+    thumbnail_path = os.path.join(THUMBNAIL_DIR, f"{memberno}.png")
+    # 썸네일 저장
+    image.save(thumbnail_path, format="PNG")
+    return thumbnail_path
 
 
 @app.get("/")
