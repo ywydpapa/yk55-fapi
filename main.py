e01f36d4e81185d0dc8ab98045b40c6c3b7942b4
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2026-01-21 11:52:53 +0900

    OTP 사용설정

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from urllib import request
+import status
 import uvicorn
 from fastapi import FastAPI, Depends, Request, Form, Response, HTTPException, File, UploadFile, Body
 from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
@@ -22,6 +23,7 @@ from fastapi.responses import StreamingResponse
 from io import BytesIO
 from starlette.responses import FileResponse
 from pathlib import Path
+import secrets
 
 dotenv.load_dotenv()
 DATABASE_URL = os.getenv("dburl")
@@ -71,6 +73,42 @@ async def save_thumbnail(image_data: bytes, memberno: int, size=(100, 100)):
     return thumbnail_path
 
 
+async def generate_otp():
+    return str(secrets.randbelow(10 ** 9)).zfill(9)
+
+
+async def reg_otp(otp:str, userNo:int, db:AsyncSession = Depends(get_db)):
+    try:
+        query = text("INSERT INTO yk_seckey (userNo, otp) VALUES (:userNo, :otp)")
+        await db.execute(query, {"userNo": userNo, "otp": otp})
+        await db.commit()
+        return True
+    except Exception as e:
+        return False
+
+
+async def exp_otp(userNo:int, db:AsyncSession = Depends(get_db)):
+    try:
+        now = datetime.datetime.now()
+        query = text("UPDATE yk_seckey SET expDate = now(), attrib = :xup where userNo = :userNo and attrib = :xapp")
+        await db.execute(query, {"userNo": userNo, "xapp": '1000010000', "xup": 'XXXUPXXXUP'})
+        await db.commit()
+        return True
+    except Exception as e:
+        return False
+
+
+def require_login(request: Request):
+    user_no = request.session.get("user_No")
+    if not user_no:
+        raise HTTPException(
+            status_code=status.HTTP_303_SEE_OTHER,
+            headers={"Location": "/"},
+            detail="세션이 만료되어 재로그인이 필요합니다."
+        )
+    return user_no
+
+
 @app.get("/", response_class=HTMLResponse)
 async def login_form(request: Request):
     if request.session.get("user_No"):
@@ -92,27 +130,34 @@ async def login_post(
     user = result.fetchone()
     if user is None:
         return templates.TemplateResponse("login/login.html",{"request": request, "error": "Invalid credentials"})
+    otp = await generate_otp()
     # 서버 세션에 사용자 ID 저장
     request.session["user_No"] = user[0]
     request.session["user_Name"] = user[1]
     request.session["user_Role"] = user[2]
     request.session["user_Region"] = user[3]
     request.session["user_Clubno"] = user[4]
+    request.session["otp"] = otp
     return RedirectResponse(url="/success", status_code=303)
 
 # 로그인 성공 페이지
-@app.get("/success", response_class=HTMLResponse)
-async def success_page(request: Request):
+@app.get("/success",response_class=HTMLResponse)
+async def success_page(request: Request,db:AsyncSession = Depends(get_db)):
     user_No = request.session.get("user_No")
     user_Name = request.session.get("user_Name")
     user_Role = request.session.get("user_Role")
     user_region = request.session.get("user_Region")
     user_clubno = request.session.get("user_Clubno")
+    otp = request.session.get("otp")
+    otpstat = await reg_otp(otp,user_No,db)
+    msg = ""
+    if otpstat is False:
+        msg = "OTP 등록 실패"
     if not user_No:
         return RedirectResponse(url="/")
     return templates.TemplateResponse("main/basic.html",
                                       {"request": request, "user_No": user_No, "user_Name": user_Name,
-                                       "user_Role": user_Role, "user_region": user_region, "user_clubno": user_clubno})
+                                       "user_Role": user_Role, "user_region": user_region, "user_clubno": user_clubno, "otp": otp, "message": msg})
 
 
 @app.post("/changeuserpass")
@@ -132,9 +177,15 @@ async def favicon():
 
 
 @app.get("/logout")
-async def logout(request: Request):
-    request.session.clear()  # 세션 삭제
-    return RedirectResponse(url="/")
+async def logout(request: Request, db: AsyncSession = Depends(get_db)):
+    try:
+        user_No = request.session.get("user_No")
+        await exp_otp(user_No, db)
+        request.session.clear()  # 세션 삭제
+        return RedirectResponse(url="/")
+    except Exception as e:
+        return RedirectResponse(url="/", status_code=303)
+
 
 
 # User
